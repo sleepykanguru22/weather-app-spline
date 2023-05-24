3fdd1bf9b9b528f661b828f66a5da8deb7434190
Jasmin Carter <70289696+sleepykanguru22@users.noreply.github.com>
2023-05-24 16:40:22 -0700

    scene not responding

--- src/App.js
+++ src/App.js
@@ -2,17 +2,17 @@ import './App.css';
 import './components/backgroundScene.css'
 import Weather from './components/Weather'
 import Clock from './components/Clock';
-import BgScreen from './components/BgScreen';
+import NScene from './components/NScene';
+
 function App() {
   return (
-    <div className="App test">
-        <BgScreen/>
+    <div className="App">
+        <NScene/>
         <div className="card text-bg-dark bg-opacity-25 mb-3 centered">
         <Clock/>
         <Weather/>
          <footer>
           This project was coded by Jasmin Carter and is partly open-sourced on <span><a href="https://github.com/sleepykanguru22" target="_blank" rel="noreferrer">github</a></span>
-          
         </footer>
         </div>
     </div>
---
+++ src/components/NScene.js
@@ -0,0 +1,7 @@
+import Spline from '@splinetool/react-spline';
+
+export default function NScene() {
+  return (
+    <Spline scene="https://prod.spline.design/RqQvQKIwWNlohumT/scene.splinecode" className="bg-scene"/>
+  );
+}
